3104bbc09ba47780d6dfc86030de808693999811
Konstantinos Benos <mpenos.ks@gmail.com>
2021-07-02 17:57:09 +0300

    Create Transform struct

--- OpenGLApplication/OpenGLApplication/src/utils/Types.h
+++ OpenGLApplication/OpenGLApplication/src/utils/Types.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 struct Color
 {
@@ -6,4 +8,11 @@ struct Color
 	float g;
 	float b;
 	float a;
+};
+
+struct Transform
+{
+	glm::vec3 position;
+	glm::quat rotation;
+	glm::vec3 scale;
 };
