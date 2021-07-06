34b8faf8c4433c11acd4959434a1994c79501cca
Konstantinos Benos <mpenos.ks@gmail.com>
2021-07-06 19:45:05 +0300

    Finalize basic version of Mesh class

--- OpenGLApplication/OpenGLApplication/src/core/Mesh.cpp
+++ OpenGLApplication/OpenGLApplication/src/core/Mesh.cpp
@@ -1,11 +1,41 @@
+#include <glad/glad.h>
 #include "Mesh.h"
 
-Mesh::Mesh()
+Mesh::Mesh(std::vector<VertexData> data, std::vector<unsigned int> indices)
 {
+	this->vertexData = data;
+	this->indices = indices;
 
+	this->SetupMesh();
 }
 
 Mesh::~Mesh()
 {
+	
+}
 
+void Mesh::SetupMesh()
+{
+	glGenVertexArrays(1, &this->VAO);
+	glGenBuffers(1, &this->VBO);
+	glGenBuffers(1, &this->EBO);
+
+	glBindVertexArray(this->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData) * this->vertexData.size(), this->vertexData.data(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * this->indices.size(), this->indices.data(), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, normal));
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+}
+
+void Mesh::DrawMesh() const
+{
+	glBindVertexArray(this->VAO);
+	glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
 }
--- OpenGLApplication/OpenGLApplication/src/core/Mesh.h
+++ OpenGLApplication/OpenGLApplication/src/core/Mesh.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <vector>
+#include "Shader.h"
+#include "../utils/Types.h"
 
 class Mesh
 {
+private:
+	// Add support for textures later
+	std::vector<VertexData> vertexData;
+	std::vector<unsigned int> indices;
+
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+
+	void SetupMesh();
+
 public:
-	Mesh();
+	Mesh() = delete;
+	Mesh(std::vector<VertexData> data, std::vector<unsigned int> indices);
 	virtual ~Mesh();
+
+	void DrawMesh() const;
 };
