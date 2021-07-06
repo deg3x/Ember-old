d5a6afbfb553cb976d37cca671d952d24256b865
Konstantinos Benos <mpenos.ks@gmail.com>
2021-07-06 20:19:04 +0300

    Create Sphere data getters

--- OpenGLApplication/OpenGLApplication/src/utils/Sphere.h
+++ OpenGLApplication/OpenGLApplication/src/utils/Sphere.h
@@ -20,4 +20,14 @@ public:
 
 	void GenerateVertexData();
 	void GenerateIndices();
+
+	inline std::vector<VertexData> GetVertexData()
+	{
+		return this->vertexData;
+	}
+
+	inline std::vector<unsigned int> GetIndices()
+	{
+		return this->indices;
+	}
 };
