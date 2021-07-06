987f296f03fb8e353d5e9af629756e7b89f4a7f3
Konstantinos Benos <mpenos.ks@gmail.com>
2021-07-06 16:57:34 +0300

    Finalize basic version of Transform

--- OpenGLApplication/OpenGLApplication/src/core/Transform.cpp
+++ OpenGLApplication/OpenGLApplication/src/core/Transform.cpp
@@ -2,10 +2,44 @@
 
 Transform::Transform()
 {
+	this->position = glm::vec3(0.0f, 0.0f, 0.0f);
+	this->rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+	this->scale = glm::vec3(1.0f, 1.0f, 1.0f);
+}
 
+Transform::Transform(glm::vec3 initPosition, glm::vec3 initRotation, glm::vec3 initScale)
+{
+	this->position = initPosition;
+	this->rotation = initRotation;
+	this->scale = initScale;
 }
 
 Transform::~Transform()
 {
 
 }
+
+glm::vec3 Transform::GetForwardVector() const
+{
+	glm::vec3 forward;
+
+	forward.x = glm::cos(glm::radians(this->rotation.y)) * glm::cos(glm::radians(this->rotation.x));
+	forward.y = glm::sin(glm::radians(this->rotation.x));
+	forward.z = glm::sin(glm::radians(this->rotation.y)) * glm::cos(glm::radians(this->rotation.x));
+
+	return glm::normalize(forward);
+}
+
+glm::vec3 Transform::GetRightVector() const
+{
+	return glm::normalize(glm::cross(this->GetForwardVector(), Transform::worldUp));
+}
+
+glm::vec3 Transform::GetUpVector() const
+{
+	return glm::normalize(glm::cross(Transform::worldRight, this->GetForwardVector()));
+}
+
+const glm::vec3 Transform::worldForward = glm::vec3(0.0f, 0.0f, 1.0f);
+const glm::vec3 Transform::worldRight = glm::vec3(1.0f, 0.0f, 0.0f);
+const glm::vec3 Transform::worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
--- OpenGLApplication/OpenGLApplication/src/core/Transform.h
+++ OpenGLApplication/OpenGLApplication/src/core/Transform.h
@@ -4,13 +4,27 @@
 
 class Transform
 {
-private:
+public:
 	glm::vec3 position;
-	glm::quat rotation;
+	glm::vec3 rotation; // Switch to quaternions later
 	glm::vec3 scale;
 
-public:
+	glm::vec3 forward;
+	glm::vec3 right;
+	glm::vec3 up;
+
+	static const glm::vec3 worldForward;
+	static const glm::vec3 worldRight;
+	static const glm::vec3 worldUp;
+
 	Transform();
+	Transform(glm::vec3 initPosition, glm::vec3 initRotation, glm::vec3 initScale);
 	virtual ~Transform();
 
+	//void SetRotationEuler(glm::vec3 newRotationEuler);
+	//glm::vec3 GetRotationEuler() const;
+
+	glm::vec3 GetForwardVector() const;
+	glm::vec3 GetRightVector() const;
+	glm::vec3 GetUpVector() const;
 };
