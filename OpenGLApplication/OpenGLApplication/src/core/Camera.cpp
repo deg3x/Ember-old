00b76ab4444ad2888db9ecdd0487cebce112b420
Konstantinos Benos <mpenos.ks@gmail.com>
2021-07-06 21:13:16 +0300

    Change Camera default values

--- OpenGLApplication/OpenGLApplication/src/core/Camera.cpp
+++ OpenGLApplication/OpenGLApplication/src/core/Camera.cpp
@@ -2,7 +2,7 @@
 
 Camera::Camera()
 {
-	transform = Transform(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 180.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	transform = Transform(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, -90.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
 }
 
 Camera::~Camera()
