e0a45e0a1c2884fa9fc9f8b46c904382ee0265d2
Konstantinos Benos <mpenos.ks@gmail.com>
2023-09-29 00:47:58 +0200

    Group ImGui examples, delete editor.ini and add to gitignore

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -79,6 +79,50 @@ namespace
 		scene.AddObject(transpSphere);
 		scene.AddObject(dirLightObject);
 	}
+
+	void DrawExamples(bool draw, const ImGuiIO& io)
+	{
+		if(!draw)
+		{
+			return;
+		}
+
+		
+		bool show_another_window = true;
+		bool showDemo = true;
+		ImGui::ShowDemoWindow(&showDemo);
+		float color[3];
+		
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");								// Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");					// Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &showDemo);					// Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);     // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", color);	// Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))								// Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+			ImGui::End();
+		}
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+	}
 }
 
 Editor::Editor()
@@ -139,7 +183,6 @@ void Editor::Tick()
 
 	ImGuiIO& io = ImGui::GetIO();
 	
-	bool show_another_window = true;
 	ImVec4 clear_color = ImVec4(0.16f, 0.15f, 0.18f, 1.00f);
 
 	viewportFB->Bind();
@@ -157,41 +200,8 @@ void Editor::Tick()
 			tabs[i]->Tick();
 		}
 
-		bool showDemo = true;
-		ImGui::ShowDemoWindow(&showDemo);
-		
-		{
-			static float f = 0.0f;
-			static int counter = 0;
+		DrawExamples(false, io);
 
-			ImGui::Begin("Hello, world!");								// Create a window called "Hello, world!" and append into it.
-
-			ImGui::Text("This is some useful text.");					// Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &showDemo);					// Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);     // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color);	// Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))								// Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-			ImGui::End();
-		}
-
-		engineWindow->SetClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
 		ImGui::Render();
 
 		const MouseData mouse = engineWindow->GetMouseData();
@@ -205,6 +215,7 @@ void Editor::Tick()
 		scene.GetCamera()->GetParent()->transform->position.y = radius * glm::cos(phi);
 
 		engineWindow->ResetMouseOffsetData();
+		engineWindow->SetClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 		engineWindow->Clear();
 
 		scene.Tick();
