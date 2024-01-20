357c4c63e44ca0d9744620ae8952a3cd00d840bd
Konstantinos Benos <mpenos.ks@gmail.com>
2024-01-20 20:29:29 +0100

    Create Screen class

---
+++ Engine/screen/Screen.cpp
@@ -0,0 +1,30 @@
+﻿#include "Screen.h"
+
+#include <string>
+#include <glfw/glfw3.h>
+
+std::string Screen::GetScreenName()
+{
+    return glfwGetMonitorName(glfwGetPrimaryMonitor());
+}
+
+std::vector<VideoMode> Screen::GetAvailableVideoModes()
+{
+    int count;
+    const GLFWvidmode* mode = glfwGetVideoModes(glfwGetPrimaryMonitor(), &count);
+
+    std::vector<VideoMode> ret;
+
+    for (int i = 0; i < count; i++)
+    {
+        VideoMode currentMode = {};
+        
+        currentMode.width     = mode[i].width;
+        currentMode.height    = mode[i].height;
+        currentMode.framerate = mode[i].refreshRate;
+
+        ret.emplace_back(currentMode);
+    }
+
+    return ret;
+}
---
+++ Engine/screen/Screen.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Definitions.h"
+#include <string>
+#include <vector>
+
+struct VideoMode
+{
+    int width;
+    int height;
+    int framerate;
+};
+
+class ENGINE_API Screen
+{
+public:
+    Screen() = delete;
+
+    static std::string GetScreenName();
+    static std::vector<VideoMode> GetAvailableVideoModes();
+};
