d5a6a7c19fb993457c7b36bd71580b31bfc882fa
Konstantinos Benos <mpenos.ks@gmail.com>
2024-02-13 01:06:56 +0100

    Fix overlapping lines and improve grid

--- Engine/shaders/fragmentEditorGrid.glsl
+++ Engine/shaders/fragmentEditorGrid.glsl
@@ -12,13 +12,13 @@ uniform vec3 cameraPosition;
 void main()
 {
     const float thicknessNorm = 0.50f; // grid line size (in pixels)
-    const float thicknessBold = 1.5 * thicknessNorm;
-    const float frequency     = 10.0;
+    const float thicknessBold = 2.0 * thicknessNorm;
+    const float frequencyBold = 10.0;
     const float maxCameraDist = 100.0;
-    const vec3 colorDefault = vec3(0.70, 0.70, 0.70);
-    const vec3 colorBold    = vec3(0.85, 0.85, 0.85);
-    const vec3 colorAxisX   = vec3(0.85, 0.40, 0.30);
-    const vec3 colorAxisZ   = vec3(0.40, 0.50, 0.85);
+    const vec3 colorDefault   = vec3(0.70, 0.70, 0.70);
+    const vec3 colorBold      = vec3(0.85, 0.85, 0.85);
+    const vec3 colorAxisX     = vec3(0.85, 0.40, 0.30);
+    const vec3 colorAxisZ     = vec3(0.40, 0.50, 0.85);
     
     // Grid Test Normal
     vec2 derivative = fwidth(WorldPos.xz);
@@ -33,9 +33,9 @@ void main()
     float alphaGridNorm = clamp(gridNorm, 0.0, 0.8);
     
     // Grid Test Bold
-    derivative = fwidth(WorldPos.xz / frequency);
+    derivative = fwidth(WorldPos.xz / frequencyBold);
     gridAA     = derivative * 1.5;
-    gridUV     = 1.0 - abs(fract(WorldPos.xz / frequency) * 2.0 - 1.0);
+    gridUV     = 1.0 - abs(fract(WorldPos.xz / frequencyBold) * 2.0 - 1.0);
     lineWidth  = thicknessBold * derivative;
     drawWidth  = clamp(lineWidth, derivative, vec2(0.5));
     gridTest   = 1.0 - smoothstep(drawWidth - gridAA, drawWidth + gridAA, gridUV);
@@ -48,7 +48,7 @@ void main()
     float alphaGrid = max(alphaGridNorm, alphaGridBold);
     
     // Color Test
-    vec3 colorOutput = step(gridBold, gridNorm) * colorDefault + step(gridNorm, gridBold) * colorBold;
+    vec3 colorOutput = mix(colorDefault, colorBold, gridBold);
     
     float alignAxisX = step(abs(WorldPos.z), gridTest.y);
     float alignAxisZ = step(abs(WorldPos.x), gridTest.x);
