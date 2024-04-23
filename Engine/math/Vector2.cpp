3c20518d0da52b5b302685048ac74225511497ad
Konstantinos Benos <mpenos.ks@gmail.com>
2024-04-23 20:04:52 +0200

    Complete fundamental Vector2 functionality

--- Engine/math/Vector2.cpp
+++ Engine/math/Vector2.cpp
@@ -1,2 +1,56 @@
 #include "engine_pch.h"
 #include "Vector2.h"
+
+#include "Math.h"
+
+float Vector2::Dot(const Vector2& lhs, const Vector2& rhs)
+{
+    return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+float Vector2::Length(const Vector2& vector)
+{
+    return Sqrt(LengthSqr(vector));
+}
+
+float Vector2::LengthSqr(const Vector2& vector)
+{
+    return vector.x * vector.x + vector.y * vector.y;
+}
+
+Vector2 Vector2::Normalize(const Vector2& vector)
+{
+    return vector / Length(vector);
+}
+
+Vector2& Vector2::operator+=(const Vector2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+
+    return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+
+    return *this;
+}
+
+Vector2& Vector2::operator*=(float rhs)
+{
+    x *= rhs;
+    y *= rhs;
+
+    return *this;
+}
+
+Vector2& Vector2::operator/=(float rhs)
+{
+    x /= rhs;
+    y /= rhs;
+
+    return *this;
+}
--- Engine/math/Vector2.h
+++ Engine/math/Vector2.h
@@ -6,4 +6,52 @@ struct ENGINE_API Vector2
 {
     real x;
     real y;
+
+    static real Dot(const Vector2& lhs, const Vector2& rhs);
+    static real Length(const Vector2& vector);
+    static real LengthSqr(const Vector2& vector);
+    static Vector2 Normalize(const Vector2& vector);
+    
+    Vector2& operator+=(const Vector2& rhs);
+    Vector2& operator-=(const Vector2& rhs);
+
+    Vector2& operator*=(real rhs);
+    Vector2& operator/=(real rhs);
 };
+
+inline Vector2 operator+(const Vector2& lhs, const Vector2& rhs)
+{
+    Vector2 ret = lhs;
+
+    ret += rhs;
+    
+    return ret;
+}
+
+inline Vector2 operator-(const Vector2& lhs, const Vector2& rhs)
+{
+    Vector2 ret = lhs;
+
+    ret -= rhs;
+    
+    return ret;
+}
+
+inline Vector2 operator/(const Vector2& lhs, real rhs)
+{
+    Vector2 ret = lhs;
+
+    ret /= rhs;
+    
+    return ret;
+}
+
+inline Vector2 operator/(real lhs, const Vector2& rhs)
+{
+    Vector2 ret;
+
+    ret.x = lhs / rhs.x;
+    ret.y = lhs / rhs.y;
+    
+    return ret;
+}
