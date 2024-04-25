8a5adf7744e56af9db407134d4b6b5a9279c8b43
Konstantinos Benos <mpenos.ks@gmail.com>
2024-04-26 01:49:18 +0200

    Add precomputed common math variables

--- Engine/math/Math.h
+++ Engine/math/Math.h
@@ -2,6 +2,12 @@
 
 #include "Definitions.h"
 
+constexpr real PI      = 3.1415926535897932384626433832795;
+constexpr real TWO_PI  = 2.0 * PI;
+constexpr real HALF_PI = 0.5 * PI;
+constexpr real RAD2DEG = 180.0 / PI;
+constexpr real DEG2RAD = PI / 180.0;
+
 real ENGINE_API Sqrt(real value);
 real ENGINE_API FastInvSqrtApproxOne(real value);
 
