fab30a1d7229b376a660ff1c6d363d569791d9b6
Konstantinos Benos <mpenos.ks@gmail.com>
2024-08-02 07:12:32 +0200

    Extend math pi definitions

--- Engine/math/Math.h
+++ Engine/math/Math.h
@@ -6,8 +6,6 @@
     constexpr real PI       = 3.1415926535897932384626433832795;
     constexpr real TWO_PI   = 6.28318530717958647692;
     constexpr real HALF_PI  = 1.57079632679489661923;
-    constexpr real RAD2DEG  = 180.0 / PI;
-    constexpr real DEG2RAD  = PI / 180.0;
     constexpr real REAL_MIN = 2.2250738585072014e-308;
     constexpr real REAL_MAX = 1.7976931348623158e+308;
     constexpr real EPSILON  = 2.2204460492503131e-016;
@@ -15,12 +13,18 @@
     constexpr real PI       = 3.1415926535897932f;
     constexpr real TWO_PI   = 6.28318530717f;
     constexpr real HALF_PI  = 1.57079632679f;
-    constexpr real RAD2DEG  = 180.0f / PI;
-    constexpr real DEG2RAD  = PI / 180.0f;
     constexpr real REAL_MIN = 1.175494351e-38f;
     constexpr real REAL_MAX = 3.402823466e+38f;
     constexpr real EPSILON  = 1.192092896e-07f;
 #endif
+
+constexpr real PI_SQR      = PI * PI;
+constexpr real PI_CUBE     = PI * PI * PI;
+constexpr real INV_PI_SQR  = static_cast<real>(1) / PI_SQR;
+constexpr real INV_PI_CUBE = static_cast<real>(1) / PI_CUBE;
+constexpr real INV_PI      = static_cast<real>(1) / PI;
+constexpr real RAD2DEG     = static_cast<real>(180) / PI;
+constexpr real DEG2RAD     = PI / static_cast<real>(180);
 
 real ENGINE_API Abs(real value);
 real ENGINE_API Sign(real value);
